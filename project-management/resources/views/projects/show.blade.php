c14750e43172252c9a4841ce9134896714937e24
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-03-14 23:29:21 +0100

    devo solo sistemare la grafica

--- project-management/resources/views/projects/show.blade.php
+++ project-management/resources/views/projects/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ $project->title }}
+            {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
@@ -9,23 +9,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-                    <p class="text-white">{{ $project->description }}</p>
-                    <p class="text-white">Linguaggi usati: 
-                        @if (is_array($project->languages_used))
-                            {{ implode(', ', $project->languages_used) }}
-                        @else
-                            {{ $project->languages_used }}
-                        @endif
-                    </p>
-                    <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-primary text-white">Modifica</a>
-                    <form action="{{ route('projects.destroy', $project->id) }}" method="POST" style="display: inline-block;">
-                        @csrf
-                        @method('DELETE')
-                        <h1 class="text-white"><button type="submit" class="btn btn-danger">Elimina</button></h1>
-                    </form>
+                    <!-- Contenuto della dashboard -->
+                    <a href="{{ route('projects.create') }}" class="text-white ">CREA NUOVO PROGETTO!</a>
+                    <h3><a href="{{ route('projects.index') }}" class="text-white ">I MIEI PROGETTI!</a></h3>
+                    <h3 class="text-lg font-semibold mb-4 text-white">Elenco Progetti</h3>
+                    <ul class="list-disc text-white">
+                        @foreach($projects as $project)
+                            <li>{{ $project->title }}</li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
-
