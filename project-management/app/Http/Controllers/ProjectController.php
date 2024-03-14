240c225f8e83751390e3ae1a094cdadc0a794323
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-03-14 19:56:00 +0100

    problema hash login risolto

--- project-management/app/Http/Controllers/ProjectController.php
+++ project-management/app/Http/Controllers/ProjectController.php
@@ -31,7 +31,7 @@ class ProjectController extends Controller
     {
         return view('projects.create');
     }
-
+//mio
     // Salva un nuovo progetto
     public function store(Request $request)
     {
