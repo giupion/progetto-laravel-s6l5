2e425559424bc3a759dae749c2fa336572e3b2e8
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-03-14 21:48:29 +0100

    done

--- project-management/resources/views/projects/index.blade.php
+++ project-management/resources/views/projects/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Elenco Progetti') }}
+            {{ __('ELENCO PROGETTI PERSONALI') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-                    <ul>
+                    <h1>I miei progetti:</h1>
+                <ul>
                         @foreach($projects as $project)
                             <li><a href="{{ route('projects.show', $project->id) }}">{{ $project->title }}</a></li>
                         @endforeach
