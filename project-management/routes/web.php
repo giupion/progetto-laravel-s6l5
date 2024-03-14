0adb1e4213af081c5331c323c2708534a5bfb78e
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-03-14 18:58:02 +0100

    done

--- project-management/routes/web.php
+++ project-management/routes/web.php
@@ -3,14 +3,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/', [ProjectController::class, 'index'])->name('dashboard'); // Modifica la route per la dashboard per visualizzare l'elenco dei progetti
+    Route::get('/', [ProjectController::class, 'index'])->name('dashboard'); 
     Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
     Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
     Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
     Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
     Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
     Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index'); // Aggiunta la route per l'elenco dei progetti
 });
 
 require __DIR__.'/auth.php';
-
