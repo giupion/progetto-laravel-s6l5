47f7b9f95346674da25d02c7a2a9bcb35de4a687
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-03-14 22:10:47 +0100

    done

--- project-management/resources/views/projects/show.blade.php
+++ project-management/resources/views/projects/show.blade.php
@@ -9,19 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-                    <p>{{ $project->description }}</p>
-                    <p>Linguaggi usati: 
+                    <p class="text-white">{{ $project->description }}</p>
+                    <p class="text-white">Linguaggi usati: 
                         @if (is_array($project->languages_used))
                             {{ implode(', ', $project->languages_used) }}
                         @else
                             {{ $project->languages_used }}
                         @endif
                     </p>
-                    <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-primary">Modifica</a>
+                    <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-primary text-white">Modifica</a>
                     <form action="{{ route('projects.destroy', $project->id) }}" method="POST" style="display: inline-block;">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Elimina</button>
+                        <h1 class="text-white"><button type="submit" class="btn btn-danger">Elimina</button></h1>
                     </form>
                 </div>
             </div>
