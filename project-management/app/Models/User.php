ad7b6a44a1f94e6a139175641319bd44ee3caf54
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-03-17 20:07:35 +0100

    done

--- project-management/app/Models/User.php
+++ project-management/app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
 
-//
+//miiiiiiiiii
 class User extends Authenticatable
 {
     use Notifiable;
