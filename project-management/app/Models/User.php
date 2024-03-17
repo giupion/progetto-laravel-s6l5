09c124560866d46d829321ae57142fc9215e2e42
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-03-17 19:47:42 +0100

    done

--- project-management/app/Models/User.php
+++ project-management/app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
-use SebastianBergmann\CodeCoverage\Report\Xml\Project;
+
 //
 class User extends Authenticatable
 {
